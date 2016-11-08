fb792f681aeb9b41739dfc69fc874761623b2e27
U-GLOBALSERVS\alejo.ribes <alejo.ribes@es-mad-wklw1148.globalservs.com>
2016-11-08 15:55:02 +0100

    add tags options, compare and count tags

--- jsontoarray.js
+++ jsontoarray.js
@@ -1,21 +1,42 @@
-//Convert in array all objects in JSON 
+//Convert in array all objects in JSON and compare and count  tags
 //Dependencies:
 // count2word  npm i -S count2word
 
 //read file
-var tags = fs.readFileSync('options/tags.json', 'utf8');
-// parse string of file
-var jsonTags = JSON.parse(tags);
-//empty array
-var arrayTags = [];
-//loops to revise and push all properties of object
-for (var key in jsonTags) {
-    // skip loop if the property is from prototype
-    if (!jsonTags.hasOwnProperty(key)) continue;
+var fs = require('fs');
+const count2Word = require('count2word');
 
-    var obj = jsonTags[key];
-    for (var prop in obj) {
-       arrayTags.push(obj[prop]);
-    }
+
+var tags = fs.readFileSync('options/tags.json', 'utf8');
+var arrayTags = getArrayTags(tags)
+
+
+
+var contents = fs.readFileSync('textTest.txt', 'utf8');
+
+var arrayWords = count2Word(contents);
+
+var affinities = [];
+
+for (i in arrayTags){
+	if(arrayWords[arrayTags[i]]){
+		affinities.push(arrayTags[i] + " " + arrayWords[arrayTags[i]]);
+
+	}
 }
-console.log(arrayTags);
+console.log(affinities);
+
+
+function getArrayTags(jsonTag){	
+	var jsonTags = JSON.parse(tags);
+	var arrayTags = [];
+	for (var key in jsonTags) {
+	    // skip loop if the property is from prototype
+	    if (!jsonTags.hasOwnProperty(key)) continue;
+	    	var obj = jsonTags[key];
+		    for (var prop in obj) {
+		       arrayTags.push(obj[prop]);
+		    }
+	}
+	 return arrayTags;	
+}
